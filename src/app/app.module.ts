61b71b332d6ab011a7453469fbdf37e79c0374b9
ElectiveRob <robstortelers1990@gmail.com>
2017-06-10 11:07:54 +0200

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import 'hammerjs';
+import { MdSidenavModule, MdListModule, MdButtonModule } from '@angular/material';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdSidenavModule,
+    MdListModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
